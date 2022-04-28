8b9731a9ec9711ced942212c389b9fb322575867
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2022-04-28 18:40:55 +0300

    Add files via upload

---
+++ BurgerApp/Models/MyListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerApp.Models
+{
+    public class MyListModel
+    {
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public string Image { get; set; }
+        public string Ingredients { get; set; }
+    }
+}
